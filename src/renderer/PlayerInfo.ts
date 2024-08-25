c4db3ec4bde2aec8149c0fdacbf31378e138e7f8
atomy <atomy@jackinpoint.net>
2024-08-25 06:18:40 +0200

    - init

---
+++ src/renderer/PlayerInfo.ts
@@ -0,0 +1,34 @@
+export interface PlayerInfo {
+  Name: string;
+  SteamID: string;
+  Connected: string;
+  Ping: number;
+  Loss: number;
+  State: string;
+  LastSeen: number;
+  MemberType: string;
+  Type: string;
+  Team: string;
+  IsMe: boolean;
+  TF2Class: string;
+  SteamProfileDataLoaded: string;
+  SteamTF2DataLoaded: string;
+  SteamBanDataLoaded: string;
+  SteamBanCommunityBanned: boolean;
+  SteamBanNumberOfGameBans: number;
+  SteamBanEconomyBan: string;
+  SteamBanVACBanned: boolean;
+  SteamBanVACBans: number;
+  SteamBanDaysSinceLastBan: number;
+  SteamTF2Playtime: number;
+  SteamURL: string;
+  SteamAvatarSmall: string;
+  SteamAvatarMedium: string;
+  SteamAvatarFull: string;
+  SteamVisible: number;
+  SteamCreatedTimestamp: number;
+  SteamCountryCode: string;
+  SteamConfigured: number;
+  PlayerWarningReason: string;
+  PlayerWarningType: string;
+}
