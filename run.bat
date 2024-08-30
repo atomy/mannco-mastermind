add2501bb2bae233628caeee3379dd2a6d718197
atomy <atomy@jackinpoint.net>
2024-08-31 01:59:38 +0200

    - add startup script

---
+++ run.bat
@@ -0,0 +1,10 @@
+SET STEAM_KEY=XXX
+SET TF2_LOGPATH=M:\\SteamLibrary\\steamapps\\common\\Team Fortress 2\\tf\\console.log
+SET "MONGODB_URI=mongodb+srv://xxx/?retryWrites=true&w=majority"
+SET MONGODB_NAME=TeamFortress2
+SET ENABLE_AUTOBALANCE_COMMENT=1
+SET OPENAI_APIKEY=sk-xxx
+SET TF2_RCON_AUTOSTART=1
+SET AUTO_OPEN_DEVTOOLS=0
+
+npm start
